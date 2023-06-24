e8e98f3de85ec82fed19cf21bd44928edba02b72
Daniel Andrei Salipot <danielsalipot@gmail.com>
2023-06-25 01:20:56 +0800

    Posts CRUD blade templates

--- resources/views/admin/post/create.blade.php
+++ resources/views/admin/post/create.blade.php
@@ -1,1 +1,12 @@
 @extends('layouts.app')
+
+@section('content')
+<form action="/admin/post" method="post" enctype="multipart/form-data">
+    @csrf
+    <input type="hidden" name="user_id" value="1">
+    <input type="text" name="title">
+    <textarea name="description"></textarea>
+    <input type="file" name="images[]" multiple accept="images/*">
+    <button type="submit">submit</button>
+</form>
+@endsection
--- resources/views/admin/post/edit.blade.php
+++ resources/views/admin/post/edit.blade.php
@@ -1,1 +1,35 @@
 @extends('layouts.app')
+
+@section('content')
+@if ($errors->count())
+    @foreach ($errors as $err)
+        {{ $err }}
+    @endforeach
+@endif
+@foreach ($post->assets as $asset)
+    <img src="{{ asset($asset->link) }}" style="width: 100px; height: 100px;">
+@endforeach
+<p>Revisions: {{ $post->revisions }}</p>
+<form action="/admin/post/{{ $post->id }}" method="post" enctype="multipart/form-data">
+    @csrf
+    @method('PUT')
+    <input type="hidden" name="user_id" value="1">
+    <input type="text" name="title" value="{{ $post->title }}"><br>
+    <textarea name="description">{{ $post->description }}</textarea><br>
+
+    <small>status</small>
+    <input type="checkbox" name="status" {{ $post->status ? 'checked' : '' }} value="1"><br>
+
+    <small>visibility</small>
+    <input type="checkbox" name="visibility" {{ $post->visibility ? 'checked' : '' }} value="1"><br>
+    <input type="file" name="images[]" multiple accept="images/*"><br>
+
+    <button type="submit">submit</button>
+</form>
+
+<form action="/admin/post/{{ $post->id }}" method="post">
+    @csrf
+    @method('DELETE')
+    <button type="submit">delete</button>
+</form>
+@endsection
