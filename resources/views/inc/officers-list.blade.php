753d80b0d6bbffac81c2fa50323a4e1d8ce9aa32
ian.ray.duyao <ian.ray.duyao@adamson.edu.ph>
2023-08-21 19:54:05 +0800

    officers again

--- resources/views/inc/officers-list.blade.php
+++ resources/views/inc/officers-list.blade.php
@@ -12,7 +12,7 @@
             <p class="text-xl font-bold">Sylvester Shaun D. Seño</p>
             <p class="text-sm italic">MArch-CTM, FRIA, Assoc AIA, ICOMOS, PGBI</p>
             <hr>
-            <p class="text-md">Chairman</p>
+            <p class="text-md"><b>Chairman</b></p>
         </div>
     </div>
     <div class="text-center lg:p-12 min-[320px]:p-8">
@@ -22,7 +22,7 @@
             </div>
             <p class="text-xl font-bold">Edgardo L. Mallari</p>
             <p class="text-sm italic">UAP, CBP</p>
-            <p class="text-md">Deputy Chair</p>
+            <p class="text-md"><b>Deputy Chair</b></p>
         </div>
     </div>
     <div class="text-center lg:p-12 min-[320px]:p-8">
@@ -32,7 +32,7 @@
             </div>
             <p class="text-xl font-bold">Armando T. Tantoco</p>
             <p class="text-sm italic">UAP</p>
-            <p class="text-md">Finance Director</p>
+            <p class="text-md"><b>Finance Director</b></p>
         </div>
     </div>
     <div class="text-center lg:p-12 min-[320px]:p-8">
@@ -42,7 +42,7 @@
             </div>
             <p class="text-xl font-bold">Alfredo Bautista</p>
             <p class="text-sm italic">UAP, Greeen ADP, EDGE</p>
-            <p class="text-md">Programs Director</p>
+            <p class="text-md"><b>Programs Director</b></p>
         </div>
     </div>
     <div class="text-center lg:p-12 min-[320px]:p-8">
@@ -52,7 +52,7 @@
             </div>
             <p class="text-xl font-bold">Carmelito Tatlonghari</p>
             <p class="text-sm italic">MMP, LEED-AP, BD+C</p>
-            <p class="text-md">Programs Deputy Director & Green Building Rating Standards Founding Member</p>
+            <p class="text-md"><b>Programs Deputy Director & Green Building Rating Standards Founding Member</b></p>
         </div>
     </div>
     <div class="text-center lg:p-12 min-[320px]:p-8">
@@ -62,7 +62,8 @@
             </div>
             <p class="text-xl font-bold">Andy Reformado</p>
             <p class="text-sm italic">UAP</p>
-            <p class="text-md">Auditor</p>
+            <p class="text-md"><b>Auditor</b>
+            </p>
         </div>
     </div>
     <div class="text-center lg:p-12 min-[320px]:p-8">
@@ -72,16 +73,16 @@
             </div>
             <p class="text-xl font-bold">Rex S. Chua Tak</p>
             <p class="text-sm italic">UAP</p>
-            <p class="text-md">Logistics Director</p>
+            <p class="text-md"><b>Logistics Director</b></p>
         </div>
     </div>
     <div class="text-center lg:p-12 min-[320px]:p-8">
         <div class="flex flex-col items-center text-muesli">
             <div style="width: 250px; height: 250px; margin-bottom: 5%; border-radius: 50%; overflow: hidden;">
-                <img src="/assets/images/Blank-Profile.jpg">
+                <img src="/assets/images/ATTY PUNZALAN EDITED-min.png">
             </div>
             <p class="text-xl font-bold">Atty. Dulce Punzalan</p>
-            <p class="text-md">Special Projects Director</p>
+            <p class="text-md"><b>Special Projects Director</b></p>
         </div>
     </div>
     <div class="text-center lg:p-12 min-[320px]:p-8">
@@ -91,17 +92,17 @@
             </div>
             <p class="text-xl font-bold">Cedel S. Austria</p>
             <p class="text-sm italic">UAP, RMP</p>
-            <p class="text-md">Operations and Informations Technology  Director</p>
+            <p class="text-md"><b>Operations and Informations Technology  Director</b></p>
         </div>
     </div>
     <div class="text-center lg:p-12 min-[320px]:p-8">
         <div class="flex flex-col items-center text-muesli">
             <div style="width: 250px; height: 250px; margin-bottom: 5%; border-radius: 50%; overflow: hidden;">
-                <img src="/assets/images/DSC08355-min.JPG">
+                <img src="/assets/images/Ar Edgar Reformado edited-min.png">
             </div>
             <p class="text-xl font-bold">Edgardo V. Reformado</p>
             <p class="text-sm italic">FUAP</p>
-            <p class="text-md">Emeritus Chairperson / Founding Chairperson / Council of Masters</p>
+            <p class="text-md"><b>Emeritus Chairperson / Founding Chairperson / Council of Masters</b></p>
         </div>
     </div>
     <div class="text-center lg:p-12 min-[320px]:p-8">
@@ -111,17 +112,17 @@
             </div>
             <p class="text-xl font-bold">Mike Peña</p>
             <p class="text-sm italic">UAP</p>
-            <p class="text-md">Business Relations Director / Founding Member / Council of Masters</p>
+            <p class="text-md"><b>Business Relations Director / Founding Member / Council of Masters</b></p>
         </div>
     </div>
     <div class="text-center lg:p-12 min-[320px]:p-8">
         <div class="flex flex-col items-center text-muesli">
             <div style="width: 250px; height: 250px; margin-bottom: 5%; border-radius: 50%; overflow: hidden;">
-                <img src="/assets/images/Blank-Profile.jpg">
+                <img src="/assets/images/Ar Leo Parinas edited-min.png">
             </div>
             <p class="text-xl font-bold">Leo Pariñas</p>
             <p class="text-sm italic">UAP, Int'l Assoc AIA</p>
-            <p class="text-md">Business Relations</p>
+            <p class="text-md"><b>Business Relations</b></p>
         </div>
     </div>
     <div class="text-center lg:p-12 min-[320px]:p-8">
@@ -131,17 +132,17 @@
             </div>
             <p class="text-xl font-bold">Maria Luisa Daya-Garcia</p>
             <p class="text-sm italic">UAP, Intl Assoc AIA, AA, CBP, ASSURE, PIArb Assoc</p>
-            <p class="text-md">Immediate Past Chairperson</p>
+            <p class="text-md"><b>Immediate Past Chairperson</b></p>
         </div>
     </div>
     <div class="text-center lg:p-12 min-[320px]:p-8">
         <div class="flex flex-col items-center text-muesli">
             <div style="width: 250px; height: 250px; margin-bottom: 5%; border-radius: 50%; overflow: hidden;">
-                <img src="/assets/images/Blank-Profile.jpg">
+                <img src="/assets/images/Aris Basa edited-min.png">
             </div>
             <p class="text-xl font-bold">Aristotle Basa</p>
             <p class="text-sm italic">UAP, PIA, Int'l Assoc AIA</p>
-            <p class="text-md">Continuing Professional Development Director</p>
+            <p class="text-md"><b>Continuing Professional Development Director</b></p>
         </div>
     </div>
     <div class="text-center lg:p-12 min-[320px]:p-8">
@@ -151,17 +152,38 @@
             </div>
             <p class="text-xl font-bold">Christian Guerrero</p>
             <p class="text-sm italic">FUAP, AA</p>
-            <p class="text-md">Special Projects Director </p>
+            <p class="text-md"><b>Special Projects Director</b> </p>
         </div>
     </div>
     <div class="text-center lg:p-12 min-[320px]:p-8">
         <div class="flex flex-col items-center text-muesli">
             <div style="width: 250px; height: 250px; margin-bottom: 5%; border-radius: 50%; overflow: hidden;">
-                <img src="/assets/images/Blank-Profile.jpg">
+                <img src="/assets/images/DSC08683-min.JPG">
             </div>
             <p class="text-xl font-bold">Jerry U. Chun</p>
             <p class="text-sm italic">UAP., NAMPAP., PIFPO., EAROPH. FIRA., FRIMPb., GreenAP., STE., PGBI+ADP</p>
-            <p class="text-md">Membership Director</p>
+            <p class="text-md"><b>Membership Director</b></p>
+        </div>
+    </div>
+    
+    <div class="text-center lg:p-12 min-[320px]:p-8">
+        <div class="flex flex-col items-center text-muesli">
+            <div style="width: 250px; height: 250px; margin-bottom: 5%; border-radius: 50%; overflow: hidden;">
+                <img src="/assets/images/Lorenzo Estoye edited-min.png">
+            </div>
+            <p class="text-xl font-bold">Lorenzo M. Estoye</p>
+            <p class="text-sm italic">UAP</p>
+            <p class="text-md"><b>Mentorship Director</b></p>
+        </div>
+    </div>
+
+    <div class="text-center lg:p-12 min-[320px]:p-8">
+        <div class="flex flex-col items-center text-muesli">
+            <div style="width: 250px; height: 250px; margin-bottom: 5%; border-radius: 50%; overflow: hidden;">
+                <img src="/assets/images/Cusi_Ernest Andrei T-min.JPG">
+            </div>
+            <p class="text-xl font-bold">Ernest Cusi</p>
+            <p class="text-md"><b>Digital Media Designer</b></p>
         </div>
     </div>
 
@@ -173,21 +195,11 @@
     <div class="text-center lg:p-12 min-[320px]:p-8">
         <div class="flex flex-col items-center text-muesli">
             <div style="width: 250px; height: 250px; margin-bottom: 5%; border-radius: 50%; overflow: hidden;">
-                <img src="/assets/images/madulid.png">
+                <img src="/assets/images/Toni Madulid Png-min.png">
             </div>
             <p class="text-xl font-bold">Toni Dominique B. Madulid</p>
-            <p class="text-md">Director</p>
+            <p class="text-md"><b>Director</b></p>
         </div>
     </div>
 
-{{--    <div class="text-center lg:p-12 min-[320px]:p-8">--}}
-{{--        <div class="flex flex-col items-center text-muesli">--}}
-{{--            <div style="width: 250px; height: 250px; margin-bottom: 5%; border-radius: 50%; overflow: hidden;">--}}
-{{--                <img src="/assets/images/Blank-Profile.jpg">--}}
-{{--            </div>--}}
-{{--            <p class="text-xl font-bold">Lorenzo M. Estoye</p>--}}
-{{--            <p class="text-sm italic">UAP</p>--}}
-{{--            <p class="text-md">Mentorship Director</p>--}}
-{{--        </div>--}}
-{{--    </div>--}}
 </div>
