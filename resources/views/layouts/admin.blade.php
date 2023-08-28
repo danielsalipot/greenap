e0449ac547ed44a0d5d6ff61a745b1fcde9eb24a
kennethleanda <leandakenneth@gmail.com>
2023-08-28 20:36:17 +0800

    admin logout

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -43,7 +43,7 @@
                             <span class="flex-1 ml-10 text-left whitespace-nowrap" sidebar-toggle-item>Posts </span>
                             <svg sidebar-toggle-item class="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
                         </button>
-                        <ul id="post-drop" class="hidden py-2 space-y-2 px-10">
+                        <ul id="post-drop" class="hidden py-2 space-y-2 px-10 ">
                             <li>
                                 <a href="/admin/post" class="flex items-center w-full p-2 text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">Drafts</a>
                             </li>
@@ -67,14 +67,19 @@
                             </li>
                         </ul>
                     </ul>
-                  </li>
-                  <li>
-                    <a href="/message" class="flex items-center w-full p-2 text-gray-900 transition duration-75 rounded-lg group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">
-                       <span class="flex-1 ml-3"><i class="fa-regular fa-envelope me-2"></i> Messages</span></a>
-                  </li>
-               </ul>
+                    </li>
+                    <li>
+                        <a href="/message" class="flex items-center w-full p-2 text-gray-900 transition duration-75 rounded-lg group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">
+                        <span class="flex-1 ml-3"><i class="fa-regular fa-envelope me-2"></i> Messages</span></a>
+                    </li>
+                    <hr class="border border-t-1 border-muesli border-dashed mx-4 ">
+                    <li>
+                        <a href="/admin/logout" class="flex items-center w-full p-2 text-gray-900 transition duration-75 rounded-lg group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">
+                        <span class="flex-1 ml-3"><i class="fa-solid fa-arrow-right-from-bracket me-2"></i>Logout</span></a>
+                    </li>
+                </ul>
             </div>
-         </aside>
+        </aside>
 
 
 
